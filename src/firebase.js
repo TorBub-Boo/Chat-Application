37f6c878004496fc19a13562ca1c39017e916084
TorBub-Boo <acaraphasawet.p@outlook.com>
2024-06-12 18:56:58 +0700

    Commit message

--- src/firebase.js
+++ src/firebase.js
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 const firebaseConfig = {
-    apiKey: "AIzaSyCr89tryLCDib-KOC4e6aleWni8iBdxfTo",
-    authDomain: "chat-application-8ff7d.firebaseapp.com",
-    projectId: "chat-application-8ff7d",
-    storageBucket: "chat-application-8ff7d.appspot.com",
-    messagingSenderId: "1000522788422",
-    appId: "1:1000522788422:web:6db8ac69aaf60d5439e447",
-    measurementId: "G-P0CQLB2403"
+    apiKey: "AIzaSyCC8oj-AQQ9w2fhKIs2hbTauDBhVIJV-EE",
+    authDomain: "chat-4d75c.firebaseapp.com",
+    projectId: "chat-4d75c",
+    storageBucket: "chat-4d75c.appspot.com",
+    messagingSenderId: "700836607006",
+    appId: "1:700836607006:web:63b68bdb7068f233d3aa0f",
+    measurementId: "G-TY2SZ9PEL4"
 };
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
